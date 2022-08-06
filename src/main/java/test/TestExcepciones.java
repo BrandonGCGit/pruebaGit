dcded139a386ce9fa237867e204f3b9f0150ff31
Brand_Git <brandgc1516@gmail.com>
2022-08-05 22:12:01 -0600

    Segundo comit del proyecto

--- src/main/java/test/TestExcepciones.java
+++ src/main/java/test/TestExcepciones.java
@@ -4,20 +4,20 @@ import static eccepciones.Aritmetica.division;
 
 public class TestExcepciones {
     public static void main(String[] args) {
-        //int resultado = 0;
+        int resultado = 0;
 
-        //try {
-        //    resultado = division(10,0);
-        //}
-        //catch (Exception exception){
-        //    System.out.println("Ocurrio un error");
-        //    exception.printStackTrace(System.out);
-        //}
+        try {
+            resultado = division(10,0);
+        }
+        catch (Exception exception){
+            System.out.println("Ocurrio un error");
+            exception.printStackTrace(System.out);
+        }
         
-        int x = 19;
-        int y = 18;
-        
-        String resultado = x>y? "Soy mayor de edad" : "Soy menor de edad";
-        System.out.println("resultado = " + resultado);
+       // int x = 19;
+       // int y = 18;
+       //
+       // string resultado = x>y? "Soy mayor de edad" : "Soy menor de edad";
+       // system.out.println("resultado = " + resultado);
     }
 }
